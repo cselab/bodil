85df44e7d39f3ddaca41aa25917f2de9612da202
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-01-22 11:34:28 -0500

    minor

--- test/test_HMC.py
+++ test/test_HMC.py
@@ -9,13 +9,14 @@ from uq_odil.HMC import HMC
 def main():
     device = 'cpu'
     dim = 1
-    num_samples = 10000
+    num_samples = 5000
     sig = 2.0
+    burnin = 10
 
     x = torch.zeros(dim, requires_grad=True)
     sigma = torch.FloatTensor([sig] * dim)
 
-    hmc = HMC([x], dt=0.05, L=10, M=1.0)
+    hmc = HMC([x], dt=0.08 * sig, L=10, M=0.5 * sig)
 
     def closure():
         hmc.zero_grad()
@@ -25,9 +26,10 @@ def main():
 
     samples = []
 
-    for k in range(num_samples):
+    for k in range(num_samples + burnin):
         x_, H = hmc.step(closure)
-        samples.append(x_[0].detach().numpy())
+        if k >= burnin:
+            samples.append(x_[0].detach().numpy())
 
     samples = np.array(samples)
 
@@ -36,10 +38,12 @@ def main():
     p = np.exp(-x**2 / (2 * sig**2)) / np.sqrt(2 * np.pi * sig**2)
 
     fig, ax = plt.subplots()
-    ax.hist(samples[:,0], range=(-r, r), bins=50, density=True)
-    ax.plot(x, p, '-r')
+    ax.hist(samples[:,0], range=(-r, r), bins=50, density=True, label='Hamiltonian MC')
+    ax.plot(x, p, '-r', label='exact')
     ax.set_xlim(-r, r)
     ax.set_ylim(0, 1.3 * np.max(p))
+    ax.set_xlabel(r'$x$')
+    ax.legend()
     plt.show()
 
 if __name__ == '__main__':
