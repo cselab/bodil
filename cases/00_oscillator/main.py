d3804c3755e835f1d54cb81692e7ea91da38d283
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-01-22 13:05:45 -0500

    wip: oscillator

---
+++ cases/00_oscillator/main.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.optim import Adam
+
+def generate_data(num_data, T, omega, x0, v0, rng, sigma):
+    """
+    Parameters:
+        num_data: number of samples to generate
+        T: time horizon
+        omega: frequency of the oscillator
+        x0: initial position
+        v0: initial velocity
+        rng: numpy random number generator
+        sigma: noise level
+    """
+    t = rng.uniform(0, T, num_data)
+    x = v0 / omega * np.sin(omega * t) + x0 * np.cos(omega * t) + rng.normal(0, sigma, num_data)
+    return t, x
+
+
+def main():
+    # a particle of mass m with position x and velocity v, in harmonic force -k x.
+
+    T = 20.0
+    k = 1.0
+    m = 1.0
+    omega = np.sqrt(k / m)
+    x0 = 0.5
+    v0 = 0.2
+
+    seed = 2349873
+    num_epochs = 2500
+    lr = 1e-3
+    num_data = 20
+    lambda_data = 1e-2
+    sigma_data = 0.1
+    a0 = 1.0
+    rng = np.random.default_rng(seed=seed)
+
+    td, xd = generate_data(num_data, T, omega=omega, x0=x0, v0=v0, rng=rng, sigma=sigma_data)
+
+    nt = 32
+
+    t = np.linspace(0, T, nt + 1, endpoint=True)
+    dt = t[1] - t[0]
+    y = torch.zeros((nt + 1, 2), requires_grad=True)
+
+    td_ids = torch.from_numpy((td / dt).astype(int))
+    xd = torch.from_numpy(xd)
+
+    optim = Adam([y], lr=lr)
+
+    def compute_loss():
+        x = y[:,0]
+        v = y[:,1]
+        dxdt = torch.diff(x) / dt
+        dvdt = torch.diff(v) / dt
+        xm = (x[:-1] + x[1:]) / 2
+        vm = (v[:-1] + v[1:]) / 2
+
+        ode1_res = dxdt - vm
+        ode2_res = dvdt + omega * xm
+        data_res = x[td_ids] - xd
+
+        loss = torch.mean(ode1_res**2) + torch.mean(ode2_res**2)
+        loss += lambda_data * torch.mean(data_res**2)
+        return loss
+
+    epochs = list(range(num_epochs))
+    losses = []
+    for epoch in epochs:
+        optim.zero_grad()
+        loss = compute_loss()
+        loss.backward()
+        optim.step()
+        l = float(loss.detach().cpu().float())
+        if epoch % 1000 == 0:
+            print(f"epoch {epoch:06d}, loss {l:.6e}")
+
+        losses.append(l)
+
+
+    xexact = v0/omega * np.sin(omega * t) + x0 * np.cos(omega * t)
+    fig, ax = plt.subplots()
+    ax.plot(t, xexact, '--b')
+    ax.plot(t, y[:,0].detach().numpy(), '-k')
+    ax.plot(td, xd.detach().numpy(), '+r')
+    ax.set_xlabel(r"$t$")
+    ax.set_ylabel(r"$x$")
+    ax.set_xlim(0, T)
+    plt.show()
+
+    plt.close(fig)
+
+    fig, ax = plt.subplots()
+    ax.plot(epochs, losses)
+    ax.set_xlabel("epoch")
+    ax.set_ylabel("loss")
+    #ax.set_yscale('log')
+    plt.show()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
