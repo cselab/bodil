010bf6c524e4013ed6d92d5161786138aaf1b02b
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-02-18 16:29:38 -0500

    poc/multigrid: add torch version

--- poc/multigrid/main_torch.py
+++ poc/multigrid/main_torch.py
@@ -2,11 +2,12 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 def upscale_grid(u):
     dim = np.ndim(u)
     new_shape = tuple(2 * s for s in u.shape)
-    uf = np.zeros(new_shape, dtype=u.dtype)
+    uf = torch.zeros(new_shape, dtype=u.dtype)
 
     # Copy original points
     uf[tuple([slice(None, None, 2)] * dim)] = u
@@ -15,7 +16,7 @@ def upscale_grid(u):
     for axis in range(dim):
         slices1 = tuple(slice(None, -1, 2) if i == axis else slice(None) for i in range(dim))
         slices2 = tuple(slice(1, None, 2) if i == axis else slice(None) for i in range(dim))
-        uf[slices2] = (uf[slices1] + np.roll(uf[slices1], shift=-1, axis=axis)) / 2
+        uf[slices2] = (uf[slices1] + torch.roll(uf[slices1], shifts=-1, dims=axis)) / 2
 
     # Remove edges.
     return uf[tuple([slice(0, -1)] * dim)]
@@ -25,15 +26,15 @@ def main():
     nx = 5
     L = 2 * np.pi
 
-    x = np.linspace(0, L, nx)
-    u = np.sin(x)
+    x = torch.linspace(0, L, nx)
+    u = torch.sin(x)
 
     uf = upscale_grid(upscale_grid(u))
-    xf = np.linspace(0, L, len(uf))
+    xf = torch.linspace(0, L, len(uf))
 
     fig, ax = plt.subplots()
-    ax.plot(x, u)
-    ax.plot(xf, uf, 'o')
+    ax.plot(x.numpy(), u.numpy())
+    ax.plot(xf.numpy(), uf.numpy(), 'o')
     ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$u$')
     plt.show()
