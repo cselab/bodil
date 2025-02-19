965329dbc64d342b6b0dc0d75bfa9df92e0afea2
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-02-19 16:16:35 -0500

    uq: compute beta from sigma

--- cases/04_reaction_diffusion/uq.py
+++ cases/04_reaction_diffusion/uq.py
@@ -9,11 +9,18 @@ from scipy.interpolate import CubicSpline
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('csv', type=str, help='csv files that contains losses against parameter x0')
-    parser.add_argument('--beta', type=float, default=2e4, help='inverse "temperature"')
+    parser.add_argument('--sigma', type=float, default=0.05, help='data measurements error')
     args = parser.parse_args()
 
     csv_path = args.csv
-    beta = args.beta
+
+    # ODIL loss has term lambda_data * mean(delta u^2), mean over nx * ny data points.
+    # equivalent "likelihood" for data would be sum delta u^2 / 2 sigma^2, where sigma is measurements error
+    # pseudo-likelihood is exp(-beta * ODLI_loss), so we can compute beta to correspond to a given sigma.
+    nx = ny = 64
+    lambda_data = 10
+    sigma = args.sigma
+    beta = nx * ny / (2 * lambda_data * sigma**2)
 
     df = pd.read_csv(csv_path)
     df.sort_values(by='x0', inplace=True)
