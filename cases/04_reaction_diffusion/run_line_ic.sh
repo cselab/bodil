c5f1ffb7902c37949c1484a89d739653f4c9bdf9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-02-19 15:11:21 -0500

    run more cases

--- cases/04_reaction_diffusion/run_line_ic.sh
+++ cases/04_reaction_diffusion/run_line_ic.sh
@@ -2,7 +2,7 @@
 
 : ${y0=0.33333}
 
-for x0 in $(seq 0.1 0.05 0.9); do
+for x0 in $(seq 0.050 0.025 0.950); do
     echo $x0 $y0
     ./inverse.py \
          --out-dir out_inverse_x0_${x0}_y0_${y0} \
