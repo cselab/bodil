423339345e612e97b8db75d74c2c30b08d585744
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-02-25 13:57:20 -0500

    wip: extract_uq_levelsets

---
+++ cases/07_reaction_diffusion_threshold/extract_uq_levelsets.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sample_paths', type=str, nargs='+', help='paths of output directories generated from samples')
+    args = parser.parse_args()
+
+    L = 1
+    paths = args.sample_paths
+
+    fig, ax = plt.subplots()
+
+    for path in paths:
+        try:
+            with open(os.path.join(path, "u_final.npy"), "rb") as f:
+                u = np.load(f)
+                ny, nx = u.shape
+                x = np.linspace(0, L, nx)
+                y = np.linspace(0, L, ny)
+                X, Y = np.meshgrid(x, y)
+                ax.contour(X, Y, u, levels=[0.1, 0.3, 0.5, 0.7], linewidths=0.1)
+        except:
+            pass
+
+    ax.set_xlabel(r'$x$')
+    ax.set_ylabel(r'$y$')
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_aspect('equal')
+    plt.tight_layout()
+    plt.show()
+    plt.close()
+
+if __name__ == '__main__':
+    main()
