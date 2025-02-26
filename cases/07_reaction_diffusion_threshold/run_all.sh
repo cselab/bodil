da4871d3cafd253a9352a83ab412a039de7f2a3f
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-02-26 16:19:16 -0500

    run_all: create results/ dir

--- cases/07_reaction_diffusion_threshold/run_all.sh
+++ cases/07_reaction_diffusion_threshold/run_all.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+mkdir -p results
 ./forward.py
-mpirun -n 8 ./run_plane.py
+mpirun -n 8 ./run_plane.py --n 9
 ./collect_losses.py out_plane/* --out-csv results/losses.csv
 ./generate_posterior_samples.py results/losses.csv --nsamples 128 --out-csv results/samples.csv
 mpirun -n 8 ./run_samples.py results/samples.csv
