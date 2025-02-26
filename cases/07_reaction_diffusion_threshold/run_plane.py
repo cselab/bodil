f1067b61b876dcdfa441006f869b4a07ce25d4eb
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-02-26 16:16:19 -0500

    pass n as argument

--- cases/07_reaction_diffusion_threshold/run_plane.py
+++ cases/07_reaction_diffusion_threshold/run_plane.py
@@ -13,11 +13,13 @@ def main():
     parser.add_argument("--forward-dir", type=str, default="out_forward", help="output directory of forward.py")
     parser.add_argument("--base-out-dir", type=str, default="out_plane", help="base output directory")
     parser.add_argument("--threshold", type=float, default=0.5, help="Measurement threshold.")
+    parser.add_argument("--n", type=int, default=19, help="Number of points along each dimension.")
     args = parser.parse_args()
 
     forward_dir = args.forward_dir
     base_dir = args.base_out_dir
     threshold = args.threshold
+    nx = ny = args.n
 
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -26,8 +28,8 @@ def main():
     cuda_id = rank % torch.cuda.device_count()
     device = torch.device(f"cuda:{cuda_id}" if torch.cuda.is_available() else "cpu")
 
-    x0s = np.linspace(0.05, 0.95, 19)
-    y0s = np.linspace(0.05, 0.95, 19)
+    x0s = np.linspace(0.05, 0.95, nx)
+    y0s = np.linspace(0.05, 0.95, ny)
 
     Y, X = np.meshgrid(y0s, x0s)
     X = X.flatten()
