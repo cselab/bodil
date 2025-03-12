621ad6911fd5849b339abf8f25a1e0d790501594
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-03-12 12:49:28 -0400

    dump levelset contours

--- cases/07_reaction_diffusion_threshold/extract_uq_levelsets.py
+++ cases/07_reaction_diffusion_threshold/extract_uq_levelsets.py
@@ -6,17 +6,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+import pickle
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('sample_paths', type=str, nargs='+', help='paths of output directories generated from samples')
     parser.add_argument('--ground-truth', type=str, default=False, help='true concentration field (output of forward)')
+    parser.add_argument('--out-contours', type=str, default="contours.pkl", help='path to file that will contain the contours')
+    parser.add_argument('--show-plot', action='store_true', default=False, help='show plot')
     args = parser.parse_args()
 
     ground_truth_path = args.ground_truth
-    levels=[0.1, 0.3, 0.5, 0.6]
+    levels = [0.1, 0.3, 0.5, 0.6]
+    nlevels = len(levels)
     L = 1
     paths = args.sample_paths
+
+    all_segments = [[] for _ in range(nlevels)]
 
     fig, ax = plt.subplots()
 
@@ -33,6 +39,8 @@ def main():
                                   colors=[f'C{i}' for i in range(len(levels))],
                                   linewidths=0.1,
                                   vmin=0, vmax=1)
+                for i in range(nlevels):
+                    all_segments[i] += cont.allsegs[i]
         except:
             pass
 
@@ -64,8 +72,18 @@ def main():
     ax.legend(frameon=False, handlelength=1, loc='upper left')
 
     plt.tight_layout()
-    plt.show()
+    if args.show_plot:
+        plt.show()
     plt.close()
+
+    data = {"levels": levels,
+            "segments": all_segments}
+
+    if ground_truth_path:
+        data["gt_segments"] = cont.allsegs
+
+    with open(args.out_contours, "wb") as f:
+        pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
     main()
