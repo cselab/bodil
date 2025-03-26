7a35b22c5ce223c1d62b7b09e525a0ae96d4a63c
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-03-26 14:17:26 -0400

    plot_tmcmc: rescale colors

--- cases/07_reaction_diffusion_threshold/plot_tmcmc.py
+++ cases/07_reaction_diffusion_threshold/plot_tmcmc.py
@@ -14,9 +14,12 @@ def main():
 
     paths = sorted(glob.glob(os.path.join(args.basedir, "stage_???.json")))
 
-    for j, path in enumerate(paths):
-        print(path)
 
+    all_loglike = []
+    all_x0 = []
+    all_y0 = []
+
+    for path in paths:
         with open(path, "r") as f:
             data = json.load(f)
 
@@ -26,12 +29,21 @@ def main():
 
         idx = np.argsort(loglike)
 
-        x0 = samples[idx,0]
-        y0 = samples[idx,1]
-        loglike = loglike[idx]
+        all_x0.append(samples[idx,0])
+        all_y0.append(samples[idx,1])
+        all_loglike.append(loglike[idx])
+
+    all_loglike = np.array(all_loglike)
+    vmin = np.quantile(all_loglike, q=0.05)
+    vmax = np.quantile(all_loglike, q=0.95)
+
+    for stage in range(len(paths)):
+        x0 = all_x0[stage]
+        y0 = all_y0[stage]
+        ll = all_loglike[stage]
 
         fig, ax = plt.subplots()
-        ax.scatter(x0, y0, c=loglike)
+        ax.scatter(x0, y0, c=ll, vmin=vmin, vmax=vmax)
         ax.set_xlim(0, 1)
         ax.set_ylim(0, 1)
         ax.set_aspect('equal')
@@ -39,6 +51,7 @@ def main():
         ax.set_ylabel(r"$y_0$")
         plt.tight_layout()
         plt.savefig(f"stage-{stage:03d}.png")
+        plt.close(fig)
 
 
 if __name__ == '__main__':
