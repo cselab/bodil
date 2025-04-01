2ee2114720f373884ccb48dc08806c44faa21283
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-04-01 16:23:21 -0400

    plot inlet pressure

--- cases/08_pwip/main.py
+++ cases/08_pwip/main.py
@@ -11,28 +11,28 @@ import torch
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('data', type=str, help='path to .mat file containing the data')
+    parser.add_argument('--out-dir', type=str, default='out', help='path to output directory')
     args = parser.parse_args()
 
     torch.set_default_dtype(torch.float32)
 
-    out_dir = "out"
+    out_dir = args.out_dir
+    data_path = args.data
+
     os.makedirs(out_dir, exist_ok=True)
 
     num_epochs = 2000
     report_every = 100
     lr = 1e-3
     lambda_PDE = 1000
-    lambda_reg = 0
-
-    data_path = args.data
 
     data = scipy.io.loadmat(data_path)
     data_A, data_u, data_P, data_t, data_x = data['matrix_var'].T
     nt, nx = data_A.shape
 
     # rescale time and space to get mm and ms
-    l_scale = 1/1000 # 1mm
-    t_scale = 1/1000 # 1ms
+    l_scale = 1e-3 # 1mm
+    t_scale = 1e-3 # 1ms
     m_scale = 1e-6 # mg
 
     data_t /= t_scale
@@ -124,6 +124,7 @@ def main():
 
     x = data_x[0,:]
     xm = (x[:-1] + x[1:]) / 2
+    t = data_t[:,0]
 
     kp_ = kp.detach().numpy() * l_scale**3 * t_scale**2 / m_scale
 
@@ -133,6 +134,16 @@ def main():
     ax.set_ylabel(r"$k_p$ (m$^3$ s$^2$ / kg)")
     plt.tight_layout()
     plt.savefig(os.path.join(out_dir, "kp.pdf"))
+    plt.close()
+
+    inlet_P = P[:,0].detach().numpy() * m_scale / l_scale / t_scale**2
+
+    fig, ax = plt.subplots()
+    ax.plot(t, inlet_P)
+    ax.set_xlabel(r"$t$ (ms)")
+    ax.set_ylabel(r"$P$ (Pa)")
+    plt.tight_layout()
+    plt.savefig(os.path.join(out_dir, "inlet_P.pdf"))
     plt.close()
 
     t0 = data_t[0, 0]
@@ -155,8 +166,6 @@ def main():
     plt.tight_layout()
     plt.savefig(os.path.join(out_dir, "P.pdf"))
     plt.close()
-
-
 
 
 if __name__ == '__main__':
