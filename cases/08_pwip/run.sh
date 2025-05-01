b97c79a919993ae994989d9f09ae4545ab0da40f
Sergey Litvinov <slitvinov@gmail.com>
2025-05-01 14:41:23 +0200

    missing mkdir

--- cases/08_pwip/run.sh
+++ cases/08_pwip/run.sh
@@ -1,6 +1,7 @@
 find . -name '*.mat' -print0 |
     xargs -0 -n 1 --process-slot-var I -P `nproc` sh -xc '
 o=`basename $0 .mat`
+mkdir -p out/$o
 taskset --cpu-list $I python -u main.py $0 --out-dir out/$o 2>out/$o/stderr 1>out/$o/stdout
 echo $? > out/$o/status
 '
