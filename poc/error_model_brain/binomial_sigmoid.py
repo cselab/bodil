df52ba38154bedd10b8885c885003b0a9b844ded
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-05-02 15:30:30 -0400

    show relu approximation compared to sigma model

--- poc/error_model_brain/binomial_sigmoid.py
+++ poc/error_model_brain/binomial_sigmoid.py
@@ -7,6 +7,9 @@ def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
     #return 0.5 * (1 + np.tanh(x))
 
+def relu(x):
+    return np.maximum(x, 0)
+
 def main():
     u = np.linspace(0, 1, 1000)
     uc = 0.7 # threshold
@@ -20,14 +23,20 @@ def main():
         p = alpha
         loss = -np.log(alpha)
         axes[0].plot(u, loss, label=fr'$\sigma = {sigma}$')
+        relu_approx = relu(-(u-uc)/sigma)
+        axes[0].plot(u, relu_approx, '--k')
 
         p = np.exp(- loss)
         norm = np.sum((p[:-1] + p[1:]) * np.diff(u))
         p /= norm
         axes[1].plot(u, p, label=fr'$\sigma = {sigma}$')
 
+        papprox = np.exp(-relu_approx)
+        papprox /= np.sum((papprox[:-1] + papprox[1:]) * np.diff(u))
+        axes[1].plot(u, papprox, '--k')
+
     ax = axes[0]
-    ax.axvline(uc, ls='--', c='k')
+    ax.axvline(uc, ls='--', c='b')
     ax.set_xlabel(r"$u_{ij}$")
     ax.set_ylabel(r"loss for $y_{ij} = 1$")
     ax.set_xlim(0, 1)
@@ -35,7 +44,7 @@ def main():
     ax.legend()
 
     ax = axes[1]
-    ax.axvline(uc, ls='--', c='k')
+    ax.axvline(uc, ls='--', c='b')
     ax.set_xlabel(r"$u_{ij}$")
     ax.set_ylabel(r"$\alpha_{ij}$")
     ax.set_xlim(0, 1)
