8f5957d4f50a6851f1b78c1a5d2b86f6ca3515bb
Sergey Litvinov <slitvinov@gmail.com>
2025-05-08 17:57:39 +0200

    add run.sh for PINNs

---
+++ cases/08_pwip/run.sh
@@ -0,0 +1,8 @@
+find data/PWIP -name '*.mat' -print0 |
+xargs -0 -n 1 --process-slot-var I -P 4 sh -xc '
+o=`basename $0 .mat`
+mkdir -p out/$o
+cd out/$o
+CUDA_VISIBLE_DEVICES=$I python -u ../../train.cse.py ../../$0 10000 10000 100 2>stderr 1>stdout
+echo $? > status
+'
